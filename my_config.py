7d31e933511e71b9d206afce095acf292e0f05db
rggassner <rafael.gassner@gmail.com>
2025-11-27 05:46:19 -0300

    Update my_config.py

--- my_config.py
+++ my_config.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 ### Api
-user = 'api'
-password = 'apiapiapi'
+user = 'yourapiuser'
+password = 'yourapipassword'
 
+#Keep empty if not in a Multiple CMA environment
 domain_name = ''
-url = '5.2.6.1'
+#Your checkpoint management server
+url = '1.1.5.1'
+
 
 api_wait_time=1
 page_size=20
@@ -14,23 +17,36 @@ publish_wait_time=1
 OBJECT_COLOR = 'red'
 
 ### 
-target_url = "http://localhost/i/object_sync_test.json"
-auth_username = "myuser"
-auth_password = "mypassword"
+login_url = "https://urltoyourlogin"
+target_url = "https://urltoyourjsondata"
+auth_username = "usertoaccessthejson"
+auth_password = "passwordtoaccessyourjson"
 
 #Only specific groups will be synced
 RESTRICTED_GROUPS = True
-ALLOWED_SYNC_GROUPS = ['SOC_SYNC_TEST','SOC_SYNC_TEST_ESP']
+ALLOWED_SYNC_GROUPS = ['group-SOURCE','group-DESTINATION']
 
 #Run only during non working hours
-NON_WORKING_HOURS_ONLY = False
+NON_WORKING_HOURS_ONLY = True
 
 #Networks with smaller masks shouldn't be synced
 MIN_V4_MASK_LENGTH=20
 MIN_V6_MASK_LENGTH=64
 
+#Group_preffix
+GROUP_PREFIX = "group-"
+
 #Objects shouldn't be in these networks
 RAW_DISALLOWED_NETWORKS = [
-    '10.0.0.0/8',
+    '10.0.0.0/8'
     ]
 
+DUMMY_HOST = { "object_value": "192.168.253.254", "description": "Dummy host used as a placeholder in empty rules." }
+
+ENABLE_EMAIL_REPORTING = True
+
+EMAIL_SUBJECT = "Sync Report"
+EMAIL_FROM = "user@domain"
+EMAIL_TO = ["user@domain"]
+SMTP_SERVER = "smtpserver"
+SMTP_PORT = 25
